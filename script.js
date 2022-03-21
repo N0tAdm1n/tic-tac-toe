2be2e7c96db8e896fe16eb7a407dddfef686fadd
N0tAdm1n <n0tadm1n29@gmail.com>
2022-03-21 18:36:44 +0530

    PLANgit add script.js !

---
+++ script.js
@@ -0,0 +1,14 @@
+// need a gameboard module
+// - need an actual board can be done with an array
+// - send board state to render
+
+// 2 player factories
+// - have a name
+// - will be assigned either x or o
+
+// a module to render the game
+// - need to show the state of board
+
+// gamemaster - will act as gamemaster
+// - let player go turn by turn
+// - tell who won the game or if it is a draw
